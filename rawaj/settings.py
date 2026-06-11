3530ae74cac6f706d23a8b0f8881af87b2f5d59c
Waleed <waleedothman58816@gmail.com>
2026-06-11 14:17:44 +0300

    add postgresql support

--- rawaj/settings.py
+++ rawaj/settings.py
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'rawaj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3')
+    )
 }
 
 
